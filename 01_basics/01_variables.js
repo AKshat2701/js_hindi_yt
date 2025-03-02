0728adf9536e7bd5b77b636eabc134755b36177f
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-02 17:57:25 +0000

    learned variables today

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 133443;
+
+let account_Email = "Akshatsrivastava@gmail.com";
+
+var account_Password = "12345";
+
+account_City = "Jaipur";
+
+// account_Id = 123;
+account_Email = "Alsjtj@yahoo.com"
+
+account_Password = "1221"
+
+account_City = "Pratapgrah"
+
+console.table([account_Id, account_Email, account_Password , account_City]);
