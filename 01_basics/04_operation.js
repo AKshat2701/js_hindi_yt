fbaea4bdde14907a39f0f37b2042abdece7c17f8
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-09 13:30:31 +0000

    about basic conversion

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,36 @@
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+
+// console.log(2 + 3);
+// console.log(3**3);
+// console.log(2 % 3);
+
+// let str1 = "hello ";
+// let str2 = "Akshat" 
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(typeof("1" + 2));
+// console.log(typeof("1" + "23"));
+
+// console.log(1 + "21");
+
+// console.log("1" + 21 + 3);
+// console.log(1 + 3 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1)
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
