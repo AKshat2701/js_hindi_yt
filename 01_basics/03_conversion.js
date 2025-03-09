757a90625161cebfd903c4ef6f4e59574f0724e3
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-09 13:00:10 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+// let score = "stringg";
+
+// console.log(typeof(score));
+   
+// let valueInNumbers = Number(score); //write in captial letters
+// console.log(typeof(valueInNumbers));
+// console.log(valueInNumbers);
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn))
+
+let someNum = 322;
+console.log(typeof(someNum));
+let stringNumber = String(someNum)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
