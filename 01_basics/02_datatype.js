dd3f99f5a702b1265bf8dff6fa7a51318e843bf6
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-09 10:31:27 +0000

    different  datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+
+
+// alert( 3 + 3);
+// we are using node js not browser
+
+// console.log(3 + 3) ;  // code readibility should be high
+
+// console.log("Hitesh");
+
+//ecma script is the offical documentation tc39 and mdn
+
+let name = "Akshat";
+let age = 12;
+let isLoggedIn = true ;
+
+
+//number => 2^53
+//bigInt 
+//string  => " "
+//boolean => true false 
+// null => stand alone value 
+//undefined +> let state ; no defined ;
+//symbol => mainly used in React => fingma component => unique
+
+//object 
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null)); // object
+
+
