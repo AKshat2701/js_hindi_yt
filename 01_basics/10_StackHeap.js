1c6f4077793ff310654e524724da5e5958a09c01
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-10 04:15:54 +0000

    stack and Heap memory

---
+++ 01_basics/10_StackHeap.js
@@ -0,0 +1,17 @@
+//stake and Heap memory 
+//  stake on primitve datatype -> copy milta hai
+
+// heap (nonPrimitive) -> reference of original value 
+
+// let myYoutubeName = "AkshatSrivastava";
+
+// let anotherName = myYoutubeName;
+
+// console.log(myYoutubeName)
+
+// anotherName = "Harshit Srivastava"
+
+// console.log(anotherName);
+
+
+
