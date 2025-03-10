ea26bcae45b6d9420507f92d93c9ec6dfcbdf2fc
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-10 17:48:21 +0000

    learned about the strings

---
+++ 01_basics/11_strings.js
@@ -0,0 +1,23 @@
+// console.log(typeof("hello" + "Akshat "));
+
+const name = "Akshat" ;
+const repoCount = 10;
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}` 
+);
+
+const gameName = new String('HiteshHC-TAKECARE'); 
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0 , 4);
+console.log(newString);
