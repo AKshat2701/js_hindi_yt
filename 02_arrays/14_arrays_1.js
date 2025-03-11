8b56f94b43f8ed188300f555ecc2992720f0ba2e
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-11 05:43:43 +0000

    methods of array

---
+++ 02_arrays/14_arrays_1.js
@@ -0,0 +1,40 @@
+// const myArray = [12,4,55,6,7,45,3, true, "Akshat"];
+
+// console.log(myArray[0]);
+// const myHeros = ["Akshat" , "Hashit"];
+
+// console.log(myHero)
+
+//Array methods;
+
+const myArray = [1,2,3,4,5];
+// myArray.push(6);
+// console.log(myArray)
+// console.log(myArray.pop(4));
+
+// myArray.unshift(9);
+// console.log(myArray);
+
+// myArray.shift(9);
+// console.log(myArray);
+
+// console.log(myArray.includes(9));
+// console.log(typeof(myArray));
+// console.log(myArray.indexOf(2));
+
+const newArray = myArray.join()
+// console.log(newArray);
+// console.log(myArray);
+
+// console.log(typeof(newArray))
+
+//slice and splice 
+console.log("A" , myArray);
+
+const myNewArr1 = myArray.slice(1,3)
+
+console.log(myNewArr1);
+console.log("B", myArray);
+
+
+const myNewArr2 = myArray.splice(1,3) 
