ce0279f4de0983853c08e15b32da31336bca56e0
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-15 20:31:41 +0000

    Object-3

---
+++ 03_objects/18_Objects.js
@@ -0,0 +1,24 @@
+const course = {
+    courseName: "JS HINDI",
+    price: 999,
+    courseInstructor: "Akshat"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor)
+
+// console.log(instructor);
+
+// const navBar = ({company}) => {
+
+// }
+// navBar(company = "Akshat")
+
+
+
+{
+
+     
+}
