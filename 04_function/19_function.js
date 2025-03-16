74fb72d0638d8881c2eb944b56cd818ec7cb44f4
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-16 17:38:35 +0000

    functions

---
+++ 04_function/19_function.js
@@ -0,0 +1,24 @@
+
+
+// function MyName() {
+//     console.log("H");
+//     console.log("a");
+//     console.log("r");
+
+// }
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2 );
+// }
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2;
+    return result;
+    
+}
+
+
+const result = addTwoNumbers(4,5)
+
+console.log("result:" , result);
