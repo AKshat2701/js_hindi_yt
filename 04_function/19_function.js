deb6098615fbd19d2a007b10a26dbdd1f2fa787b
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-17 08:22:08 +0000

    Functions-1Repeat

--- 04_function/19_function.js
+++ 04_function/19_function.js
@@ -12,13 +12,24 @@
 //     console.log(number1 + number2 );
 // }
 
-function addTwoNumbers(number1, number2){
-    let result = number1 + number2;
-    return result;
-    
-}
+// function addTwoNumbers(number1, number2){
+//     let result = number1 + number2;
+//     return result;
+// }
 
 
-const result = addTwoNumbers(4,5)
+// const result = addTwoNumbers(4,5)
 
-console.log("result:" , result);
+// console.log("result:" , result);
+
+
+
+function loginUserMessage(userName = "sam"){
+    if(!userName){
+        console.log("Please enter a username");
+        return;
+    }
+    return `${userName} just logged in `;
+} 
+// console.log(loginUserMessage("Akshat"));
+console.log(loginUserMessage('Akshat'))
