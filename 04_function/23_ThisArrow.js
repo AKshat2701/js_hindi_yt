9a5d6cbcb689896cf07743922ce42a118b28ceb4
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-24 05:48:32 +0000

    This

---
+++ 04_function/23_ThisArrow.js
@@ -0,0 +1,15 @@
+const user = {
+    username : "hitesh",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username} ,  Welcome to website`);
+    }
+
+}
+
+// user.welcomeMessage();
+// user.username = "Sam";
+// user.welcomeMessage();
+
+// console.log(this);
