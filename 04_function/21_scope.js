239e3f539beb4470e402de42b8e930eb66816c31
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-24 04:01:16 +0000

    21Scope

--- 04_function/21_scope.js
+++ 04_function/21_scope.js
@@ -1,5 +1,43 @@
-let a = 10;
-const b = 12;
-var c = 399;
+// let a = 10;
+// const b = 12;
+// var c = 399;
 
  //scope 
+
+ //nested scope
+
+//  function one() {
+
+//     const username = "Akshat"
+
+//     function two() {
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//    console.log(website);
+//     two() ;
+//  }
+
+//  one();
+
+if(true) {
+    const username = "Hitesh"
+    if(username === "hitesh") {
+        const website = " YOUTUBE"
+        console.log(username + website );
+    }
+
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++ intresting +++++++?
+function addone(num) {
+    return num + 1;
+} 
+addone(4)
+const addTwo = function(num) {
+    return num + 3;
+}
+
+addTwo(4);
