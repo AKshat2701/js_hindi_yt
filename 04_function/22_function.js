996e5d8e964162f4ddb5915c8e950aad328b9c5c
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-03-24 04:24:56 +0000

    Scope_2

---
+++ 04_function/22_function.js
@@ -0,0 +1,35 @@
+// function one() {
+
+//     const userName = "Hitesh";
+
+//     function two() {
+//         const website = "youtube";
+//         console.log(userName);
+//     }
+//     // console.log(website);
+
+//     two();
+// }
+
+// // one();
+
+// if(true) {
+//     const username = "Akshat";
+//     if(username === "Akshat"){
+//         const website = "Akshat.com"
+
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
+// console.log(username);
+
+function addOne (num) {
+    return num + 1;
+}
+addOne(4)
+
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(29);
