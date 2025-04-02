c29630119da07dc01d8a6977443775decfac3d12
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2025-04-02 06:39:34 +0000

    Loops

--- 04_function/29_HigherOrderLoop.js
+++ 04_function/29_HigherOrderLoop.js
@@ -60,8 +60,49 @@
 //     // console.log(`${key} shortcut is for ${myObject[key]}`)
 // }
 
-const programming = ["js" , "rb" , "py", "java", 'cpp'];
+// const programming = ["js" , "rb" , "py", "java", 'cpp'];
 
-for(const key in programming){
-    console.log(programming[key]);
-}
+// for(const key in programming){
+//     console.log(programming[key]);
+// }
+
+
+//for each loop
+ 
+ const coding = ["js", "ruby", "java", "Python"];
+
+// coding.forEach(  function ( val ) {
+//     console.log(val);
+// })
+
+// coding.forEach( item() => {
+//     console.log(item);
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe())
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName: "CPP",
+        languageFileName: "c++"
+    },
+    {
+        languageName : "Python",
+        languageFileName: "Py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName); 
+})
